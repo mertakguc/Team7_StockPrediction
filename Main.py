aaffbc1f3a590061b4e89834dcb58be95aa92f3f
goktug5000 <goktug5000@gmail.com>
2024-05-07 22:23:29 +0300

    added CreateNormalizedData func

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 def ParseJSONToDataFrame(data):
     df = pd.DataFrame.from_dict(data['Time Series (Daily)'], orient='index') # parse Json to DataFrame
@@ -6,6 +8,12 @@ def ParseJSONToDataFrame(data):
     df.index = pd.to_datetime(df.index) 
     df = df.astype(float)  # converting data to float values
     return df
+
+def CreateNormalizedData(df):
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(df) #Normalized data with mix max scaler algoriythm
+    scaled_df = pd.DataFrame(scaled_data, columns=df.columns, index=df.index) # parse normalized data to df
+    return scaled_df
 
 def CreateDataset(X, y, time_steps=1):
     Xs, ys = [], []
