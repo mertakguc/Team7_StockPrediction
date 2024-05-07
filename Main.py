591d9174e2e191a973042bff421042b9d4590c9b
Mert Akgüç <70711705+mertakguc@users.noreply.github.com>
2024-05-07 22:21:04 +0300

    added CreateDataset function

--- Main.py
+++ Main.py
@@ -6,3 +6,11 @@ def ParseJSONToDataFrame(data):
     df.index = pd.to_datetime(df.index) 
     df = df.astype(float)  # converting data to float values
     return df
+
+def CreateDataset(X, y, time_steps=1):
+    Xs, ys = [], []
+    for i in range(len(X) - time_steps):
+        v = X.iloc[i:(i + time_steps)].values
+        Xs.append(v)
+        ys.append(y.iloc[i + time_steps])
+    return np.array(Xs), np.array(ys)
